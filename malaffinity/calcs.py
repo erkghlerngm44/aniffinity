4c1f8c08fc060d3f2790f08b5915a3e3a434a653
erkghlerngm44 <erkghlerngm44@protonmail.com>
2017-06-15 18:10:07 +0000

    Fix floating point shit

--- malaffinity/calcs.py
+++ malaffinity/calcs.py
@@ -1,19 +1,24 @@
 from statistics import pstdev, mean
+from decimal import Decimal
 
 
 # Original code: https://stackoverflow.com/a/17389980
 def pearson(x, y):
+    # Fix floating point shit
+    x = [Decimal(i) for i in x]
+    y = [Decimal(j) for j in y]
+
     sx = []
     sy = []
 
     # Calculate mean and stdev now so we don't have to calculate
     # it every time we come across another value.
     # Size and content of `x` and `y` shouldn't change so this should be fine.
-    mx = mean(x)
-    my = mean(y)
+    mx = Decimal(mean(x))
+    my = Decimal(mean(y))
 
-    stdx = pstdev(x)
-    stdy = pstdev(y)
+    stdx = Decimal(pstdev(x))
+    stdy = Decimal(pstdev(y))
 
     for i in x:
         sx.append((i - mx) / stdx)
@@ -21,4 +26,6 @@ def pearson(x, y):
     for j in y:
         sy.append((j - my) / stdy)
 
-    return sum([i * j for i, j in zip(sx, sy)]) / len(x)
+    r = sum([i * j for i, j in zip(sx, sy)]) / len(x)
+
+    return float(r)
