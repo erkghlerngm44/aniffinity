80f4276c166340d0c532629100fee2d838760ae8
erkghlerngm44 <erkghlerngm44@protonmail.com>
2017-06-17 16:52:36 +0000

    Add `MALAffinityException` exception base class

--- malaffinity/exceptions.py
+++ malaffinity/exceptions.py
@@ -3,7 +3,12 @@ malaffinity exceptions
 """
 
 
-class NoAffinityError(Exception):
+class MALAffinityException(Exception):
+    """Base class for MALAffinity exceptions"""
+    pass
+
+
+class NoAffinityError(MALAffinityException):
     """
     Raised when either the shared rated anime between the base user
     and another user is less than 10, or the user does not have any rated anime
@@ -11,12 +16,12 @@ class NoAffinityError(Exception):
     pass
 
 
-class InvalidUsernameError(Exception):
+class InvalidUsernameError(MALAffinityException):
     """Raised when username specified does not exist"""
     pass
 
 
-class MALRateLimitExceededError(Exception):
+class MALRateLimitExceededError(MALAffinityException):
     """
     Raised when MAL's blocking your request, because you're going over their
     rate limit of one request every two seconds. Slow down and try again
