25f22e105f7f8db330c39a0c4b807a377e9cd628
erkghlerngm44 <erkghlerngm44@protonmail.com>
2017-07-07 16:36:12 +0000

    Remove kwargs from `_retrieve_list`
    Fuck it, might as well

--- malaffinity/malaffinity.py
+++ malaffinity/malaffinity.py
@@ -52,7 +52,7 @@ class MALAffinity:
         if base_user:
             self.init(base_user)
 
-    def _retrieve_scores(self, username, status="all", type="anime"):
+    def _retrieve_scores(self, username):
         """
         Retrieve a users' animelist scores
 
@@ -63,20 +63,15 @@ class MALAffinity:
         Only anime scored > 0 will be returned, and all 
         PTW entries are ignored, even if they are scored
 
-        NOTE: `status` and `type` params may be deprecated soon(tm)
-
         :param str username: MAL username
-        :param str status: `my_status` (watching, completed, etc) of 
-            anime to return. Default is `all`. Only `all` works
-        :param str type: `anime` or `manga`
         :return: Array containing `id`, `score` pairs
         :rtype: list
         """
 
         params = {
             "u": username,
-            "status": status,
-            "type": type
+            "status": "all",
+            "type": "anime"
         }
 
         resp = requests.request("GET", self._URL, params=params)
