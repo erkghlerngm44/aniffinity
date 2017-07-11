fc0cdd75e7981297db49b30af8810acddabef757
erkghlerngm44 <erkghlerngm44@protonmail.com>
2017-07-11 16:54:20 +0000

    v2.2.0.dev0

--- malaffinity/__about__.py
+++ malaffinity/__about__.py
@@ -13,7 +13,7 @@ __title__ = "malaffinity"
 __summary__ = "Calculate affinity between MyAnimeList users"
 __uri__ = "https://github.com/erkghlerngm44/malaffinity"
 
-__version__ = "2.1.0"
+__version__ = "2.2.0.dev0"
 
 __author__ = "erkghlerngm44"
 __email__ = "erkghlerngm44@protonmail.com"
